77fddf08973487abee658d2250651fbacf77d948
0x-jerry <x.jerry.wang@gmail.com>
2023-12-08 14:23:12 +0800

    feat: reimplement add

--- src/add.ts
+++ src/add.ts
@@ -1,18 +1,23 @@
-import createMathOperation from './.internal/createMathOperation.js';
-
 /**
  * Adds two numbers.
  *
  * @since 3.4.0
  * @category Math
- * @param {number} augend The first number in an addition.
- * @param {number} addend The second number in an addition.
- * @returns {number} Returns the total.
+ * @param augend The first number in an addition.
+ * @param addend The second number in an addition.
+ * @returns Returns the total.
  * @example
  *
  * add(6, 4)
  * // => 10
  */
-const add = createMathOperation((augend, addend) => augend + addend, 0);
+function add(augend: string, addend: string): string
+function add(augend: string, addend: string | number): string
+function add(augend: string | number, addend: string): string
+function add(augend: number, addend: number): number
+function add(augend: number | string, addend: number | string): string | number {
+    // @ts-ignore
+    return augend + addend
+}
 
-export default add;
+export default add
